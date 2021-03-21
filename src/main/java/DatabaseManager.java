9fa83dba31466a46546e17bcfbe7f90263f072cf
buhinalba <buhin.alba@gmail.com>
2021-03-21 21:29:12 +0100

    add method: get connection to database using environment variables

---
+++ src/main/java/DatabaseManager.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    public static Connection getConnection() {
+        try {
+            String root = System.getenv("MARIADB_USERNAME");
+            String pw = System.getenv("MARIADB_PASSWORD");
+            String database = System.getenv("MARIADB_DATABASE_NAME");
+            String host = System.getenv("MARIADB_HOST");
+
+            if (root==null || pw==null || database==null || host==null) {
+                throw new RuntimeException("Missing Environment variables, can't connect to database!");
+            }
+
+            String url = String.format("jdbc:mariadb://%s/%s", host, database);
+            return DriverManager.getConnection(url, "root", "root");
+        } catch (SQLException e) {
+            System.out.println("Error while Connecting to database: " + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
