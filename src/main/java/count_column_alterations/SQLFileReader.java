a1d739f60d4d28e796bd9e15ac0ec05af8e33c7f
buhinalba <buhin.alba@gmail.com>
2021-03-24 12:28:53 +0100

    feat: add altered columns to list

--- src/main/java/count_column_alterations/SQLFileReader.java
+++ src/main/java/count_column_alterations/SQLFileReader.java
@@ -1,23 +1,48 @@
 package count_column_alterations;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 
 public class SQLFileReader {
     ArrayList<String> alteredColumns;
 
 
-    public ArrayList<String> readAlteredColumns(File file) {
+
+    public void readAlteredColumns(File file){
         System.out.println(file.getAbsolutePath());
-        // todo read through lines
-        // -> check for ALTER COLUMN
-        return null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
+            String line = reader.readLine();
+            while (line != null) {
+                addAlteredColumnIfExists(line);
+                // read next line
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("File does not exist: " + file.getAbsolutePath());
+        }
     }
+
 
     public SQLFileReader() {
     }
 
+
+
+    private void addAlteredColumnIfExists(String query) {
+        String[] queryParts = query.split(" ALTER COLUMN ");
+        if (queryParts.length == 2) {
+            String tableName = queryParts[0].split("ALTER TABLE ")[1];
+            String columnName = queryParts[1].split(" ")[0];
+            if (!tableName.matches(".*dx\\d{2}.*")) {
+                alteredColumns.add(tableName +"."+columnName);
+            }
+        }
+    }
+
+
     public ArrayList<String> getAlteredColumns() {
         return alteredColumns;
     }
+
 }
